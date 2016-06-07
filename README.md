6a740c682ab3d8467fed9f2545e1088407282718
Ka-Ping Yee <ping@zesty.ca>
2016-06-06 17:15:10 -0700

    Add a README file.

---
+++ README.md
@@ -0,0 +1,24 @@
+== `star_destroyer`: Eliminate `import *` from your modules ==
+
+To run this over your code, provide the root path to your files, like so:
+
+    python -m star_destroyer /path/to/files
+
+This would scan all `*.py` files anywhere under `/path/to/files`.  The path
+you provide should be a path as it would appear in `sys.path` -- thus, if you
+want to process a package, provide the path to the parent of the package
+directory, not the package directory itself.
+
+Running with just a path will print out the results of the scan and the edits
+that would be made, without actually performing the edits.  If you want
+`star_destroyer` to actually edit your files to replace all the `import *`
+statements, run it with the `-e` option, like so:
+
+    python -m star_destroyer -e /path/to/files
+
+`star_destroyer` has been tested with Python 2.7 and Python 3.5.  Run it using
+the same version of Python that your code is written for.  `PYTHONPATH` should
+also be set as it would be set during a normal run of your code; it will be
+used to find other modules that your code imports.
+
+To run the tests, execute `py.test` using Python 2.7 or Python 3.5.
--- star_destroyer.py
+++ star_destroyer.py
@@ -9,26 +9,30 @@
 # OR CONDITIONS OF ANY KIND, either express or implied.  See the License for
 # specific language governing permissions and limitations under the License.
 
-"""Eliminates 'import *' from your modules.
+"""Eliminates `import *` from your modules.
 
 To run this over your code, provide the root path to your files, like so:
 
     python -m star_destroyer /path/to/files
 
-This would scan any Python files at /path/to/files/*.py.  If you want to
-process a package, provide the path to the directory containing the package,
-not the package directory itself.  The path you provide should be a path as
-it would appear in sys.path.  PYTHONPATH should also be set as it would be
-set during a normal run of your code; it will be used to find other modules
-that your code imports.
+This would scan all `*.py` files anywhere under `/path/to/files`.  The path
+you provide should be a path as it would appear in `sys.path` -- thus, if you
+want to process a package, provide the path to the parent of the package
+directory, not the package directory itself.
 
 Running with just a path will print out the results of the scan and the edits
-that would be made, without actually performing the edits.  To actually edit
-your files, run star_destroyer with the -e option, like so:
+that would be made, without actually performing the edits.  If you want
+`star_destroyer` to actually edit your files to replace all the `import *`
+statements, run it with the `-e` option, like so:
 
     python -m star_destroyer -e /path/to/files
 
-To run the tests, execute 'py.test' using Python 2.7 or Python 3.
+`star_destroyer` has been tested with Python 2.7 and Python 3.5.  Run it using
+the same version of Python that your code is written for.  `PYTHONPATH` should
+also be set as it would be set during a normal run of your code; it will be
+used to find other modules that your code imports.
+
+To run the tests, execute `py.test` using Python 2.7 or Python 3.5.
 """
 
 from __future__ import print_function
